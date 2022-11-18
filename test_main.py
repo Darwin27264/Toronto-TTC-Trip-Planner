9420f559366b90c35830fe4df34251cf40dd49ee
Darwin27264 <47094705+Darwin27264@users.noreply.github.com>
2022-11-17 21:12:14 -0500

    A bunch of things

--- test_main.py
+++ test_main.py
@@ -1,20 +1,15 @@
 from bauhaus import Encoding, proposition, constraint
 from bauhaus.utils import count_solutions, likelihood
 
-# Some custom imports including time, SQLite， unzipper
+# Some custom imports including time, SQLite
 from datetime import datetime
 import sqlite3
 import json
-import os.path
 import geocoder
 
 # importing geopy library
 from geopy.geocoders import Nominatim
 from geopy import distance
-
-# These two lines make sure a faster SAT solver is used.
-from nnf import config
-from nnf import Var
 
 # Importing database
 data = sqlite3.connect('routes.db')
@@ -25,95 +20,6 @@ t = data_trip.cursor()
 
 data_stop = sqlite3.connect('stops.db')
 s = data_stop.cursor()
-
-config.sat_backend = "kissat"
-
-# Encoding that will store all of your constraints
-E = Encoding()
-
-
-# To create propositions, create classes for them first, annotated with "@proposition" and the Encoding
-@proposition(E)
-class BasicPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f"A.{self.data}"
-
-
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def __repr__(self):
-        return f"A.{self.data}"
-
-
-# # Budget/Price
-# kid = Var('kids/children')
-# adult = Var('adult')
-# youth = Var('youth')
-# senior = Var('senior')
-# presto = Var('presto user')
-# presto_adult = Var('presto user (adult)')
-# presto_youth = Var('presto user (youth)')
-# presto_senior = Var('presto user (senior)')
-# presto_day_pass = Var('buy presto day pass')
-# surpass_day_pass = Var('cheaper to go with day pass')
-# # Time
-# within_time_constraint = Var('within time constraint')
-# rush_hour = Var('rush hour')
-# # Additional Stops Requested
-# additional_stops = Var('additional stops')
-
-kid = BasicPropositions('kids/children')
-adult = BasicPropositions('adult')
-youth = BasicPropositions('youth')
-senior = BasicPropositions('senior')
-presto = BasicPropositions('presto user')
-presto_adult = BasicPropositions('presto user (adult)')
-presto_youth = BasicPropositions('presto user (youth)')
-presto_senior = BasicPropositions('presto user (senior)')
-presto_day_pass = BasicPropositions('buy presto day pass')
-surpass_day_pass = BasicPropositions('cheaper to go with day pass')
-
-# Time
-within_time_constraint = BasicPropositions('within time constraint')
-rush_hour = BasicPropositions('rush hour')
-# Additional Stops Requested
-additional_stops = BasicPropositions('additional stops')
-
-
-# Build an example full theory for your setting and return it.
-#
-#  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
-#  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
-#  what the expectations are.
-def example_theory():
-    # User must be fall into one of the age groups
-    E.add_constraint(adult | youth | senior | kid)
-
-    # # Add custom constraints by creating formulas with the variables you created.
-    # E.add_constraint((a | b) & ~x)
-    # # Implication
-    # E.add_constraint(y >> z)
-    # # Negate a formula
-    # E.add_constraint(~(x & y))
-    # # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    # constraint.add_exactly_one(E, a, b, c)
-
-    return E
 
 
 def get_input():
@@ -308,30 +214,7 @@ def find_closest_stop(location):
     return min_stop_id
 
 
-def main():
-    print(get_input())
-
-
-main()
-
-''' Original Test Main
-if __name__ == "__main__":
-
-    print("test")
-
-    T = example_theory()
-    # Don't compile until you're finished adding all your constraints!
-    T = T.compile()
-    # After compilation (and only after), you can check some of the properties
-    # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
-
-    print("\nVariable likelihoods:")
-    for v, vn in zip([a, b, c, x, y, z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
-'''
+test_array = get_input()
+print(test_array[0])
+# print(distance_finder((43.6950093, -79.3959279), (43.909707, -79.123111)))
+print(find_closest_stop(test_array[0]))
