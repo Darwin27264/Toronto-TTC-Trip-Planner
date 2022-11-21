2bdbd7542e9610d11fe765267dfcd4b0a0914218
jasperNie <31317861+jasperNie@users.noreply.github.com>
2022-11-20 19:05:33 -0500

    Added flexibilty across operating systems

--- test_main.py
+++ test_main.py
@@ -7,6 +7,7 @@ import sqlite3
 import json
 import geocoder
 import os
+import platform
 
 # importing geopy library
 from geopy.geocoders import Nominatim
@@ -21,6 +22,16 @@ t = data_trip.cursor()
 
 data_stop = sqlite3.connect('stops.db')
 s = data_stop.cursor()
+
+# Find OS and set clear termial command
+if platform.system() == 'Windows':
+    clearTermial = 'cls'
+elif platform.system() == 'Darwin':
+    clearTermial = 'clear'
+elif platform.system() == 'Linux':
+    clearTermial = 'clear'
+else:
+    clearTermial = 'clear'
 
 
 def distance_finder(location, ttc_stops):
@@ -142,7 +153,7 @@ def get_location_1():
                     if correct_location == "1" or correct_location == "2":
                         is_Valid = True
                     else:
-                        os.system('cls')
+                        os.system(clearTermial)
                         print("\nPlease enter either 1 or 2\n")
                         print("(1) " + getLoc_no_toronto.address)
                         print("(2) " + getLoc_toronto.address)
@@ -175,7 +186,7 @@ def get_location_2():
         return (stop[0][2], stop[0][3])
     else:
         # Clear terminal
-        os.system('cls')
+        os.system(clearTermial)
         print("\nStop does not exist in database\n")
         return False
 
@@ -215,12 +226,12 @@ def get_location_3():
                     return tuple(float(i) for i in coords.split(','))
                 else:
                     # Clear terminal
-                    os.system('cls')
+                    os.system(clearTermial)
                     print("\nCoordinates are not in Toronto\n")
                     return False
             else:
                 # Clear terminal
-                os.system('cls')
+                os.system(clearTermial)
                 print("Please enter numeric values\n")
         else:
             print("Please enter the coordinates in the correct format\n")
@@ -238,20 +249,20 @@ def get_location(str):
             input_valid = True
         else:
             # Clear terminal
-            os.system('cls')
+            os.system(clearTermial)
             print("Please enter 1, 2, or 3\n")
 
     if int(input_method) == 1:
         # Clear terminal
-        os.system('cls')
+        os.system(clearTermial)
         origin_coords = get_location_1()
     elif int(input_method) == 2:
         # Clear terminal
-        os.system('cls')
+        os.system(clearTermial)
         origin_coords = get_location_2()
     elif int(input_method) == 3:
         # Clear terminal
-        os.system('cls')
+        os.system(clearTermial)
         origin_coords = get_location_3()
     return origin_coords
 
@@ -277,7 +288,7 @@ def call_get_location(msg1, msg2, msg3):
             if correct.upper() == "Y" or correct.upper() == "N":
                 valid = True
             else:
-                os.system('cls')
+                os.system(clearTermial)
                 print("Please enter Y or N")
                 print("\n" + str(stop_name[0][1]) + " --- StopID: " + str(stop_name[0][0]) + "\n")
         if correct.upper() == "Y":
@@ -286,7 +297,7 @@ def call_get_location(msg1, msg2, msg3):
         elif correct.upper() == "N":
             near_stops = nearby_stops(stop_name[0][0])
             if len(near_stops)==0:
-                os.system('cls')
+                os.system(clearTermial)
                 print("Try a different method\n")
             else:
                 valid_Input = False
@@ -304,12 +315,12 @@ def call_get_location(msg1, msg2, msg3):
                     options.append(str(j))
                     near_input = input("\nEnter your input: ")
                     if near_input not in options:
-                        os.system('cls')
+                        os.system(clearTermial)
                         print("Please select one of the options")
                     else:
                         valid_Input = True
                 if int(near_input) == len(options):
-                    os.system('cls')
+                    os.system(clearTermial)
                     print("Try a different method\n")
                 else:
                     return near_stops[int(near_input)-1]
@@ -323,21 +334,21 @@ def validate_time(msg):
         if len(time) == 5 and ":" in time:
             hours, mins = time.split(":")[0], time.split(":")[1]
             if hours.isnumeric() == False:
-                os.system('cls')
+                os.system(clearTermial)
                 print("Hours must be numeric\n")
             elif 0 > int(hours) or int(hours) > 23:
-                os.system('cls')
+                os.system(clearTermial)
                 print("Hours must be between 0 and 23\n")
             elif mins.isnumeric() == False:
-                os.system('cls')
+                os.system(clearTermial)
                 print("Hours must be numeric\n")
             elif 0 > int(mins) or int(mins) > 59:
-                os.system('cls')
+                os.system(clearTermial)
                 print("Minutes must be between 0 and 59\n")
             else:
                 validTime = True
         else:
-            os.system('cls')
+            os.system(clearTermial)
             print("Please write in format (HH:MM)\n")
     return (int(hours), int(mins))
 
@@ -347,7 +358,7 @@ def get_start_time(str):
     validInput = False
     while validInput == False:
         timeinput = input("(1) Current Time\n(2) Specific Time\n\nEnter 1 or 2 to select: ")
-        os.system('cls')
+        os.system(clearTermial)
         if timeinput == "1" or timeinput == "2":
             validInput = True
         else:
@@ -370,7 +381,7 @@ def get_end_time(starting_time, msg, msg1):
         if check_time_after(starting_time, ending_time):
             valid_Time = True
         else:
-            os.system('cls')
+            os.system(clearTermial)
             print(msg)
     return ending_time
 
@@ -398,10 +409,10 @@ def get_age():
     while valid_Age == False:
         age = input("Enter your age (for trip price calculation): ")
         if age.isnumeric() == False:
-            os.system('cls')
+            os.system(clearTermial)
             print("Input must be a number\n")
         elif float(age) % 1 != 0:
-            os.system('cls')
+            os.system(clearTermial)
             print("Input must be an integer\n")
         else:
             valid_Age = True
@@ -415,7 +426,7 @@ def get_presto():
         if presto.upper() == "Y" or presto.upper() == "N":
             valid_Presto = True
         else:
-            os.system('cls')
+            os.system(clearTermial)
             print("Please enter either Y or N\n")
     if presto.upper() == "Y":
         return True
@@ -429,10 +440,10 @@ def get_budget():
         budget = input("Enter your spending budget: ")
         budget = budget.replace("$", "").replace(" ", "")
         if budget.isnumeric() == False:
-            os.system('cls')
+            os.system(clearTermial)
             print("Budget must be a number\n")
         elif float(budget) <= 0:
-            os.system('cls')
+            os.system(clearTermial)
             print("Budget must be more than 0\n")
         else:
             valid_Budget = True
@@ -458,7 +469,7 @@ def get_additional_stops(start_time, end_time):
         if more_stops.upper() == "Y" or more_stops.upper() == "N":
             valid_Ans = True
         else:
-            os.system('cls')
+            os.system(clearTermial)
             print("Please enter either Y or N\n")
     if more_stops.upper() == "Y":
         more = False
@@ -478,19 +489,19 @@ def get_additional_stops(start_time, end_time):
                     if check_time_before(staytime, end_time):
                         no_exceed_time = True
                     else:
-                        os.system('cls')
+                        os.system(clearTermial)
                         print("Cannot visit stop after ending stop\n")
                 minutes = time[1] + staytime[1]
                 hours = time[0] + (minutes // 60)
                 if hours >= 24:
-                    os.system('cls')
+                    os.system(clearTermial)
                     print("Cannot stay at stop until next day\n")
                 else:
                     leave_time = (hours, minutes % 60)
                     if check_time_before(leave_time, end_time):
                         no_overflow = True
                     else:
-                        os.system('cls')
+                        os.system(clearTermial)
                         print("Cannot stay at stop until after ending stop time\n")
             additional_stops.append(stop_id, (time, leave_time))
             valid_another = False
@@ -499,7 +510,7 @@ def get_additional_stops(start_time, end_time):
                 if another.upper() == "Y" or another.upper() == "N":
                     valid_another = True
                 else:
-                    os.system('cls')
+                    os.system(clearTermial)
                     print("Please enter either Y or N\n")
             if another.upper() == "N":
                 more = True
@@ -563,7 +574,7 @@ def get_input():
     start_message3 = "\nAre any of these the stop you are currently at?\n"
     start = call_get_location(start_message1, start_message2, start_message3)
 
-    os.system('cls')
+    os.system(clearTermial)
     # Gets the stop id of the destination the user want to go to
     end_message1 = "Now let's find your destination stop \n"
     end_message2 = "Is this the stop want to go to? (Y/N): "
@@ -571,39 +582,38 @@ def get_input():
     destination = call_get_location(end_message1, end_message2, end_message3)
 
     # Getting current time
-    os.system('cls')
+    os.system(clearTermial)
     print("Now let's get your trip's starting time\n")
     starting_time = get_start_time("Would you like to use the current time or indicate a specific time?")
 
     # The time user wants to get to the final destination by
 
     # user's additional stops shouldn't exceed this time, need a constraint for this
-    os.system('cls')
+    os.system(clearTermial)
     print("Now let's get your trip's ending time\n")
     string = "Ending time must be after starting time and within the same day\n"
     string2 = "Enter the time you wish to arrive at your destination (HH:MM): "
     ending_time = get_end_time(starting_time, string, string2)
 
-    os.system('cls')
+    os.system(clearTermial)
     age = get_age()
 
-    os.system('cls')
+    os.system(clearTermial)
     hasPresto = get_presto()
 
-    os.system('cls')
+    os.system(clearTermial)
     budget = get_budget()
 
-    os.system('cls')
+    os.system(clearTermial)
     additional_stops_list = get_additional_stops(starting_time, ending_time)
 
     return (start, (starting_time, starting_time)), (destination, (ending_time, ending_time)), int(
         age), hasPresto, budget, additional_stops_list
 
-
 # Clear terminal
-os.system('cls')
+os.system(clearTermial)
 test_array = get_input()
-os.system('cls')
+os.system(clearTermial)
 s.execute("SELECT * FROM stops WHERE stop_id=:stop_id", {'stop_id': test_array[0][0]})
 starting_point = s.fetchone()
 print("Starting Point: " + str(starting_point[1]) + " --- StopID: " + str(starting_point[0]) + " --- Time(Hours,Minutes): " + str(test_array[0][1]))
